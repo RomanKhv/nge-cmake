ff3db3f87e5e79be1fcd3304ae141e167a233de6
Roman <romankhvostikov@gmail.com>
2022-08-05 16:35:10 +0300

    boost dependency

---
+++ source.basic/third_party_usage/3rdparty.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+#include <boost/shared_ptr.hpp>
+
+using namespace std;
+
+int main()
+{
+	// call function from static lib "utils"
+	cout << "3rd party libs" << endl;
+
+	boost::shared_ptr<int> sp;
+
+	return 0;
+}
