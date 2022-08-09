8e8c4f156325204556874df4feab21959dd28118
FirsovAlexander <62183403+FirsovAlexander@users.noreply.github.com>
2022-08-09 14:41:41 +0300

    separated IPMPage interface usage

--- source.ui/efd_explorer/efd_explorer.cpp
+++ source.ui/efd_explorer/efd_explorer.cpp
@@ -1,10 +1,12 @@
 ﻿#include "efd_explorer.h"
-#include <iostream>
+
 #include <windows.h>
+
+#include <iostream>
+
 #include <utils.h>
-#include <pm_gui/IPMPage.h>
-#include <pm_gui/pm_gui_win.h>
-#include <pm_gui/pm_gui_mfc.h>
+#include <feature_gui.h>
+#include <pm_gui/pm_gui.h>
 #include <gui_resources.h>
 
 using namespace std;
@@ -34,14 +36,8 @@ int main()
 {
 	cout << utils::quoted("FLOEFD rules!") << endl;
 
-	if (IPMPage *page = pm_gui_win_factory())
-	{
-		cout << page->GetTitle() << endl;
-	}
-	if (IPMPage *page = pm_gui_mfc_factory())
-	{
-		cout << page->GetTitle() << endl;
-	}
+    IPMPage* page = pm_gui_factory();
+    ShowPropertyManagerPage(page);
 
 	cout << load_res_string();
 
